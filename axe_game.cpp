f8abaa97ab91fc6839e6dabe6683c14238c3d650
Jason B <jasonbetsargon@gmail.com>
2023-10-14 17:53:05 -0700

    full directional movement

--- axe_game.cpp
+++ axe_game.cpp
@@ -2,12 +2,41 @@
 
 int main()
 {
+    // window dimensions
     const int width=350;
-    InitWindow(width,200,"Axe");
+    const int height=200;
+    InitWindow(width,height,"Axe");
+
+    // circle coordinates
+    int circle_x=175;
+    int circle_y=100;
+
+    // set FPS
+    SetTargetFPS(60);
     while(!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(WHITE);
+        
+        // Game logic begins
+        DrawCircle(circle_x,circle_y,25,BLUE);
+
+        if(IsKeyDown(KEY_D))
+        {
+            circle_x+=5;
+        }
+        if(IsKeyDown(KEY_A))
+        {
+            circle_x-=5;
+        }
+        if(IsKeyDown(KEY_S))
+        {
+            circle_y+=3;
+        }
+        if(IsKeyDown(KEY_W))
+        {
+            circle_y-=3;
+        }
 
         EndDrawing();
     }
